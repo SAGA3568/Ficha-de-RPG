b13fec680922ddbd3287be0077a858a29cd0da2f
Gustavo M <muniz.gustavo@pm.me>
2024-09-29 20:31:34 -0300

    feat: upload files

---
+++ backend/resources/js/app.js
@@ -0,0 +1,1 @@
+console.warn("SAIA DAQUI, Ô CORNO");
---
+++ backend/resources/views/greeting.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>Olá! Essa página é o back-end do projeto de Pedro! Saia daqui, por favor!</h1>
+    </body>
+</html>
